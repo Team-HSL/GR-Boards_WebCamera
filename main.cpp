90578ad01d7c23a661eeb4e041358ef4c318149d
d-kato <daiki.kato.cj@renesas.com>
2017-12-26 21:20:42 +0900

    Added GR-PEACH example using GR-WIRELESS CAMERA Shield

--- main.cpp
+++ main.cpp
@@ -42,10 +42,18 @@
   LWIPBP3595Interface network;
 #elif (NETWORK_TYPE == 2)
   #include "ESP32Interface.h"
-  ESP32Interface network(P5_3, P3_14, P7_1, P0_1);
+  #if defined(TARGET_RZ_A1H)
+    ESP32Interface network(P3_10, P3_9, P2_14, P2_15, false, NC, NC, 230400);
+  #elif defined(TARGET_GR_LYCHEE)
+    ESP32Interface network(P5_3, P3_14, P7_1, P0_1, false, NC, NC, 230400);
+  #endif
 #elif (NETWORK_TYPE == 3)
   #include "ESP32InterfaceAP.h"
-  ESP32InterfaceAP network(P5_3, P3_14, P7_1, P0_1);
+  #if defined(TARGET_RZ_A1H)
+    ESP32InterfaceAP network(P3_10, P3_9, P2_14, P2_15, false, NC, NC, 230400);
+  #elif defined(TARGET_GR_LYCHEE)
+    ESP32InterfaceAP network(P5_3, P3_14, P7_1, P0_1, false, NC, NC, 230400);
+  #endif
 #else
   #error NETWORK_TYPE error
 #endif /* NETWORK_TYPE */
